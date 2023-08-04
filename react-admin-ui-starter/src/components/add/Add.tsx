f7bbc1c8460f238de8cc70f251bd33676b9b8404
Elisandra De La Cruz <ely321.edlc@outlook.com>
2023-08-04 10:39:58 +0200

    first commit

---
+++ react-admin-ui-starter/src/components/add/Add.tsx
@@ -0,0 +1,71 @@
+import { GridColDef } from "@mui/x-data-grid";
+import "./add.scss"
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+
+type Props ={
+    slug:string;
+    columns:GridColDef[]
+    setOpen:React.Dispatch<React.SetStateAction<boolean>>;
+
+};
+
+
+const Add = (props:Props) => {
+
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn:(id:number)=>{
+      return fetch(`http://localhost:8000/api/${props.slug}s`, {method:"post",
+    headers:{
+      Accept:"application/json",
+      "Content-Type":"application/json",
+    },
+    body: JSON.stringify({
+      id:111,
+      img:"",
+      lastName:"Hello",
+      firstName:"test",
+      email:"testme@gmail.com",
+      phone:"123 456 789",
+      createdAt:"01.02.2023",
+      verified:true,
+    }),
+  });
+    },
+    onSuccess: ()=>{
+      queryClient.invalidateQueries([`all${props.slug}s`]);
+    },
+  });
+
+
+    const handleSubmit=(e: React.FormEvent<HTMLFormElement>)=>{
+        e.preventDefault();
+        mutation.mutate();
+      props.setOpen(false)
+    };
+  return (
+    <div className="add">
+      <div className="modal">
+        <span className="close" onClick={()=>props.setOpen(false)}>
+            X
+        </span>
+        <h1>Add New {props.slug}</h1>
+        <form onSubmit={handleSubmit}>
+            {props.columns 
+            .filter((item)=>item.field !== "id" &&item.field !== "img") .map(columns=>(
+                <div className="item">
+                    <label>{columns.headerName}</label>
+                    <input type={columns.type} placeholder={columns.field}/>
+                </div>
+            ))}
+
+            <button>Send</button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Add
---
+++ react-admin-ui-starter/src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span> ED </span>
+      <span> Elisandra D Admin Dashboard</span>
+    </div>
+  )
+}
+
+export default Footer
